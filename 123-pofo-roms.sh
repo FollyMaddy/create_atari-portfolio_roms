bb0bb40f84cd9e982f7d35950edbf43ab5bf20f0
FollyMaddy <meulenf@gmx.net>
2021-07-23 21:10:02 +0200

    fix adding executable file to autoexec.bat
    If 1 executable is found, then this file will be added to the autoexec.bat .
    if more executable's are found, then you will be asked to type an executable which is added to the autoexec.bat .

--- 123-pofo-roms.sh
+++ 123-pofo-roms.sh
@@ -6,33 +6,34 @@ if [[ "$(id -u)" -ne 0 ]]; then
     exit 1
 fi
 
-for zip in *.zip;do
-rom=$(basename $zip .zip).rom
-#echo $rom
-cp -f 128kb-fat12.rom $rom
-mount -o loop $rom /mnt/
-unzip $zip -d /mnt/
-touch /mnt/autoexec.bat
-echo "@echo off" > /mnt/autoexec.bat
-echo
-echo "The executable filenames are listed now :"
-count=0
-ls /mnt/*.{com,exe,COM,EXE} 2>&- | while read line; do 
-count=$(($count + 1))
-echo $count $line
-filename=$line
-done
-if [[ $count=1 ]];then
-echo
-echo "One filename detected, presuming we can add this file to the autoexec.bat"
-echo "Adding $(echo $(ls /mnt/*.{com,exe,COM,EXE} 2>&-) | cut -d/ -f3 | cut -d. -f1 | cut -d. -f1)"
-echo "$(echo $(ls /mnt/*.{com,exe,COM,EXE} 2>&-) | cut -d/ -f3 | cut -d. -f1)" >> /mnt/autoexec.bat
-else
-echo
-echo "What filename do you want to add to the autoexec ?"
-echo "(You can leave the extension)"
-read -p 'filename: ' input
-echo "$input" >> /mnt/autoexec.bat
+if [[ $(ls) == *.zip* ]];then
+  for zip in *.zip;do
+    rom=$(basename $zip .zip).rom
+    #echo $rom
+    cp -f 128kb-fat12.rom $rom
+    mount -o loop $rom /mnt/
+    unzip $zip -d /mnt/
+    touch /mnt/autoexec.bat
+    echo "@echo off" > /mnt/autoexec.bat
+    echo
+    echo "The executable filenames are listed now :"
+    ls -w1 /mnt/*.{com,exe,COM,EXE} 2>&-
+    #collect executable exe or com files into array
+    #https://unix.stackexchange.com/questions/302775/write-ls-to-array-split-by-newline
+    collectfiles=()
+    readarray collectfiles < <(ls -w1 /mnt/*.{com,exe,COM,EXE} 2>&-)
+  if [[ ${#collectfiles[@]} == 1 ]];then
+    echo
+    echo "One filename detected, presuming we can add this file to the autoexec.bat"
+    echo "Adding $(echo $(ls /mnt/*.{com,exe,COM,EXE} 2>&-) | cut -d/ -f3 | cut -d. -f1 | cut -d. -f1)"
+    echo "$(echo $(ls /mnt/*.{com,exe,COM,EXE} 2>&-) | cut -d/ -f3 | cut -d. -f1)" >> /mnt/autoexec.bat
+  else
+    echo 
+    echo "What filename do you want to add to the autoexec ?"
+    echo "(You can leave the extension)"
+    read -p 'filename: ' input
+    echo "$input" >> /mnt/autoexec.bat
+  fi
+  umount /mnt/
+  done
 fi
-umount /mnt/
-done
